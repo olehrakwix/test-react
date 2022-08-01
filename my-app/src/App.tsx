336bbb53575843b7ab9ce063421ae4c3a45893ee
Oleh Rak <olehr@wix.com>
2022-08-01 13:16:15 +0300

    init

---
+++ my-app/src/App.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <div>Counter: {counter}</div>
+        <div>
+          <button onClick={() => setCounter(counter + 1)}>Click</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
