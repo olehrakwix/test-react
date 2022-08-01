c7cd27935e8db1178eaa0ce7aa118eacc5e83f44
Oleh Rak <olehr@wix.com>
2022-08-01 13:38:01 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import debounce from "debounce";
 
 function App() {
   const [counter, setCounter] = useState(0);
 
-  const handleClick = debounce(() => setCounter(counter + 1), 3000);
+  const handleClick = () => setCounter(counter + 1);
 
   return (
     <div className="App">
